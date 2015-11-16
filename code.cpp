308959d04effc7df24ea758cdc511cb185116e25
Jonathan Coe <jon@jbcoe.net>
2015-11-16 16:24:44 +0000

    class with template args

--- code.cpp
+++ code.cpp
@@ -1,3 +1,8 @@
 int short_function() { return 0; }
 
 auto pointless_tuple_builder = [](auto... ts) mutable { return std::make_tuple(ts); }; 
+
+template <typename T, typename T_ = T, typename U = some_nasty_meta_programming_trick<T_>::type>
+class MyTemplateClass : public BaseClass<T>, public U
+{
+};
--- output/JBCoe_code.cpp
+++ output/JBCoe_code.cpp
@@ -7,3 +7,9 @@ auto pointless_tuple_builder = [](auto... ts) mutable
 {
   return std::make_tuple(ts);
 };
+
+template <typename T, typename T_ = T,
+          typename U = some_nasty_meta_programming_trick<T_>::type>
+class MyTemplateClass : public BaseClass<T>, public U
+{
+};
